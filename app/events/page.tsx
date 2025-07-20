7acd790d4caa61caa803bc0c68cb491f390d6acc
Daniel Abakah <daniel.abakah@amalitech.com>
2025-07-20 13:31:21 +0000

    feat: add events parent page

---
+++ app/events/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { LayoutWrapper } from "@/components/layout-wrapper"
+import { EventsContent } from "@/components/events-content"
+
+export default function EventsPage() {
+    return (
+        <LayoutWrapper>
+            <EventsContent />
+        </LayoutWrapper>
+    );
+}
